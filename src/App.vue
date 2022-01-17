29c0dc886b4075c9982727dab0ad940b95fe4b9d
truyenpn <truyenpn@huongvietgroup.com>
2022-01-17 15:44:12 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <CardList />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CardList from './components/CardList.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CardList,
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+ display: flex;
+ justify-content: space-evenly;
+ align-items: center;
+ font-family: "Roboto", sans-serif;
+ -webkit-font-smoothing: antialiased;
+ -moz-osx-font-smoothing: grayscale;
+}
+:root {
+ font-size: 10px;
 }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="card-container">
+    <div class="card-body">
+        <p>{{item.text}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    item: {
+      tyoe: String,
+      default: '',
+    }
+  }
+}
+</script>
+
+<style scoped>
+.card-container {
+ background: #fff;
+ box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.15);
+ width: 100%;
+ height: 100%;
+ cursor: pointer;
+ margin-bottom: 2rem;
+}
+.card-body {
+ padding: 1rem 2rem;
+}
+p {
+ font-size: 1.6rem;
+}
+</style>
